54c33474e6dcea2af49bf61c602d617be9c2b589
Hans <sanpascualh73@gmail.com>
2024-12-29 20:17:04 +0800

    add kwh schema and realtime update

--- app/ui/dashboard/LineGraph.tsx
+++ app/ui/dashboard/LineGraph.tsx
@@ -10,19 +10,7 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
-import { Kwh } from "@/sanity/types";
-
-const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
-  { month: "July", desktop: 214, mobile: 140 },
-  { month: "July", desktop: 400, mobile: 400 },
-  { month: "July", desktop: 214, mobile: 140 },
-];
+// import { Kwh } from "@/sanity/types";
 
 const chartConfig = {
   desktop: {
